8b61f20eb6293546ea54ed1ad9901425dd2e1416
actuarialsnail <flynn.yiyi@gmail.com>
2021-07-08 23:09:15 +0100

    clean_up removed reporting legacy code

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ let kraken = new ccxt.kraken({
 
 let exchange_scope = {
     // coinbasepro,
-    // binance,
+    binance,
     kraken,
 };
 
@@ -401,12 +401,6 @@ const batch_request = async (exchange, req_obj) => {
 }
 
 let limits_reset = false;
-let email_sent = false;
-const email_hour = 5;
-const email_minute = 30;
-let aoc_done = false;
-const aoc_hour = 5;
-const aoc_minute = 10;
 
 main();
 
@@ -417,9 +411,6 @@ const main_timer = setInterval(async () => {
     let minute = tmstmp_current.getMinutes();
     const yesterday = new Date(tmstmp_current)
     yesterday.setDate(yesterday.getDate() - 1)
-
-    const today_date = tmstmp_current.toJSON().slice(0, 10);
-    const yesterday_date = yesterday.toJSON().slice(0, 10);
 
     if ((hour % period_h === 0) && (minute === 0)) {
         // if ((hour % 1 == 0) && (minute % 5 == 0)) {
@@ -432,149 +423,4 @@ const main_timer = setInterval(async () => {
         limits_reset = false;
     }
 
-    // scheduled daily aoc reporting process
-    // if (hour === aoc_hour && minute === aoc_minute) {
-    //     if (!aoc_done) {
-    //         aoc_done = true;
-    //         console.log('perform AoC report');
-    //         // record balance file
-    //         const balance = await coinbasepro.fetchBalance();
-    //         fs.writeFileSync('./logs/balance_' + today_date + '.json', JSON.stringify(balance));
-    //         const ticker = {};
-    //         for (const product of prouduct_scope) {
-    //             ticker[product] = await coinbasepro.fetchTicker(product);
-    //         }
-    //         fs.writeFileSync('./logs/ticker_' + today_date + '.json', JSON.stringify(ticker));
-    //         console.log('files logged');
-    //         const data_obj = await aoc(yesterday_date, today_date);
-    //         generate_html(data_obj);
-    //     }
-    // } else {
-    //     aoc_done = false;
-    // }
-
-    // scheduled daily email notification
-    // if (hour === email_hour && minute === email_minute) {
-    //     if (!email_sent) {
-    //         email_sent = true;
-    //         console.log('email AoC report');
-    //         send_mail('aoc', 'Attached.', 'report_' + today_date + '.html');
-    //     }
-    // } else {
-    //     email_sent = false;
-    // }
 }, 1000)
-
-const generate_html = (data_obj) => {
-    // console.log(data_obj);
-    const map = {
-        '{{date}}': data_obj.t1,
-        '{{value}}': data_obj.snapshot.value.toFixed(2),
-        '{{return_abs}}': data_obj.delta.transfer_abs.toFixed(2),
-        '{{return_pc}}': data_obj.delta.transfer_pc.toFixed(1),
-        '{{r_24hr_abs}}': data_obj.delta.value_abs.toFixed(2),
-        '{{r_24hr_pc}}': data_obj.delta.value_pc.toFixed(1),
-        '{{btc_value}}': data_obj.snapshot.BTC.toFixed(6),
-        '{{btc_delta_abs}}': data_obj.delta.BTC_abs.toFixed(6),
-        '{{btc_delta_pc}}': data_obj.delta.BTC_abs.toFixed(1),
-        '{{eth_value}}': data_obj.snapshot.ETH.toFixed(6),
-        '{{eth_delta_abs}}': data_obj.delta.ETH_abs.toFixed(6),
-        '{{eth_delta_pc}}': data_obj.delta.ETH_pc.toFixed(1),
-        '{{gbp_value}}': data_obj.snapshot.GBP.toFixed(2),
-        '{{gbp_delta_abs}}': data_obj.delta.GBP_abs.toFixed(2),
-        '{{gbp_delta_pc}}': data_obj.delta.GBP_pc.toFixed(1),
-        '{{transfer}}': data_obj.snapshot.transfer.toFixed(2),
-    }
-
-    fs.readFile('./template.html', 'utf8', (err, data) => {
-        if (err) {
-            return console.log(err);
-        }
-        let result = data;
-        for (const item in map) {
-            result = result.replace(item, map[item])
-        }
-        fs.writeFile('./reports/report_' + data_obj.t1 + '.html', result, 'utf8', (err) => {
-            if (err) return console.log(err);
-        });
-    });
-}
-
-const aoc = async (t0, t1) => {
-
-    try {
-        const t0_balance = require('./logs/balance_' + t0 + '.json');
-        const t0_ticker = require('./logs/ticker_' + t0 + '.json');
-        const t1_balance = require('./logs/balance_' + t1 + '.json');
-        const t1_ticker = require('./logs/ticker_' + t1 + '.json');
-
-        let data_obj = {};
-        // estimate total portfolio value
-        let t0_value = 0; let t1_value = 0;
-        let delta = {}; let snapshot = {};
-
-        for (const product of prouduct_scope) {
-            const currency = product.split('/')[0];
-            t0_value += t0_ticker[product].bid * t0_balance[currency].total;
-            t1_value += t1_ticker[product].bid * t1_balance[currency].total;
-            snapshot[currency] = t1_balance[currency].total;
-            delta[currency + '_abs'] = t1_balance[currency].total - t0_balance[currency].total;
-            delta[currency + '_pc'] = delta[currency + '_abs'] / t0_balance[currency].total;
-        }
-
-        t0_value += t0_balance[quote_currency].total;
-        t1_value += t1_balance[quote_currency].total;
-
-        snapshot[quote_currency] = t1_balance[quote_currency].total;
-        snapshot['value'] = t1_value;
-
-        delta[quote_currency + '_abs'] = t1_balance[quote_currency].total - t0_balance[quote_currency].total;
-        delta[quote_currency + '_pc'] = delta[quote_currency + '_abs'] / t0_balance[quote_currency].total;
-        delta['value_abs'] = t1_value - t0_value;
-        delta['value_pc'] = delta['value_abs'] / t0_value * 100;
-
-        // calculate total transfers in and obtain other ledger data in last 24 hrs
-        let after = ''
-        let allLedger = []
-        while (true) {
-            const limit = 100 // change for your limit
-            const params = { id: config.coinbase_dca_account_id, after, limit }
-            const trades = await coinbasepro.privateGetAccountsIdLedger(params)
-            if (trades.length) {
-                after = coinbasepro.last_response_headers['Cb-After'];
-                allLedger.push(...trades)
-            } else {
-                break
-            }
-            // console.log(`after: ${after} `)
-        }
-        fs.writeFileSync('./logs/ledger_' + t1 + '.json', JSON.stringify(allLedger));
-        let transfer_value = 0;
-        allLedger.forEach(element => {
-            // console.log(element);
-            if (element.type === 'transfer') {
-                transfer_value += Number(element.amount);
-                // console.log(element.created_at, element.amount);
-            }
-            // filter out ones in last 24hr
-
-        });
-        snapshot['transfer'] = transfer_value;
-        delta['transfer_abs'] = snapshot['value'] - transfer_value;
-        delta['transfer_pc'] = (snapshot['value'] / transfer_value - 1) * 100;
-
-        // work out total change in value % so far
-        data_obj = {
-            t0,
-            t1,
-            delta,
-            snapshot,
-        }
-        // console.log(data_obj);
-        return data_obj
-    } catch (error) {
-        console.log(error);
-        return;
-    }
-
-}
