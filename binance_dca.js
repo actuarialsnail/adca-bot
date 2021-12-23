9256d5aeba9fc07382bec7faa6f2e4a5b4abf871
actuarialsnail <flynn.yiyi@gmail.com>
2021-12-24 00:06:44 +0800

    UTC timezone update

--- binance_dca.js
+++ binance_dca.js
@@ -184,7 +184,7 @@ const main_timer = setInterval(async () => {
     let minute = tmstmp_current.getMinutes();
     let second = tmstmp_current.getSeconds();
 
-    if ((hour === 1) && (minute === 0)) {
+    if ((hour === 17) && (minute === 0)) {
         if (!limits_reset) {
             limits_reset = true;
             console.log(`===== ${tmstmp_current.toISOString()} Routine DCA triggered =====`);
